28c81102b6c9b4486458e374f4707c2a486bfb35
henrique-pizzoni <rikepizzoni@gmail.com>
2025-06-25 16:46:59 -0300

    :fire: Quadrupt features: Testando umas coisas aí

--- linear_optimization.py
+++ linear_optimization.py
@@ -1,57 +1,34 @@
-import pulp
 
+import pulp
 
 class Optimizer:
 
     def __init__(self, num_variables):
         self.num_variables = num_variables
-
         self.model = pulp.LpProblem("Maximizar_Z", pulp.LpMaximize)
-
-        self.variables = []
-
-        for i in range(self.num_variables):
-            self.variables.append(pulp.LpVariable(f'x{i + 1}', lowBound=0, cat='Continuous'))
-
+        self.variables = [pulp.LpVariable(f'x{i + 1}', lowBound=0, cat='Continuous')
+                          for i in range(num_variables)]
 
     def set_objective_function(self, coefficients):
-        objective_expression = pulp.lpSum(
-            [coefficients[i] * self.variables[i] for i in range(self.num_variables)]
-        )
-
-        self.model += objective_expression, "Objective_Function"
+        expression = pulp.lpSum([coefficients[i] * self.variables[i] for i in range(self.num_variables)])
+        self.model += expression, "Objective_Function"
 
     def add_constraint(self, coefficients, rhs, name, sense):
-        lhs_expression = pulp.lpSum(
-            [coefficients[i] * self.variables[i] for i in range(self.num_variables)]
-        )
-
+        lhs = pulp.lpSum([coefficients[i] * self.variables[i] for i in range(self.num_variables)])
         if sense == '<=':
-            self.model += lhs_expression <= rhs, name
+            self.model += lhs <= rhs, name
         elif sense == '>=':
-            self.model += lhs_expression >= rhs, name
+            self.model += lhs >= rhs, name
         elif sense == '==':
-            self.model += lhs_expression == rhs, name
+            self.model += lhs == rhs, name
 
     def solve(self):
         self.model.solve()
-
-        # Devolver pro front-end os resultados
-
-        # print("-" * 30)
-        # print("Resultados Ótimos:")
-        # for var in self.variables:
-        #     print(f"  - Valor de {var.name}: {var.varValue:.2f}")
-        #
-        # print("-" * 30)
-        # objective_value = pulp.value(self.model.objective)
-        # print(f"Valor Ótimo da Função Objetivo: {objective_value:.2f}")
-        #
-        # print("-" * 30)
-        # print("Preços Sombra (Shadow Prices):")
-        # if not self.model.constraints:
-        #     print("  - Nenhuma restrição no modelo.")
-        # else:
-        #     for name, constraint in self.model.constraints.items():
-        #         print(f"  - Restrição '{name}': {constraint.pi:.2f}")
-        # print("-" * 30)
+        valores_otimos = [v.varValue for v in self.variables]
+        valor_objetivo = pulp.value(self.model.objective)
+        precos_sombra = [c.pi for c in self.model.constraints.values()]
+        return {
+            "valores_otimos": valores_otimos,
+            "valor_objetivo": valor_objetivo,
+            "precos_sombra": precos_sombra
+        }
